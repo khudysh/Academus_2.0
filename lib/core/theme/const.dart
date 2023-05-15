25503f2c3a6b36dae6f37d38baee97acf3dc2f52
khudysh <ehd2001@mail.ru>
2023-05-15 23:57:56 +0300

    Added consts file

---
+++ lib/core/theme/const.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Const {
+  static const primaryColor = Color(0xFF1982C4);
+  static const backgroundColor = Color(0xFFF8F9FA);
+  static const secondaryColor = Color(0xFF758BFD);
+  static const tertiaryColor = Color(0xFF000000);
+  static const inactiveColor = Color(0xFF8D8D8D);
+  static const formColor = Color(0xFF6750A4);
+  static const formErrorColor = Color(0xFFB3261E);
+  static const goodColor = Color(0xFF8D8D8D);
+  static const mediumColor = Color(0xFFFFCA3A);
+  static const badColor = Color(0xFFFF595E);
+
+  static const darkPrimaryColor = Color(0xFF1982C4);
+  static const darkBackgroundColor = Color(0xFF292929);
+  static const darkSecondaryColor = Color(0xFF758BFD);
+  static const darkTertiaryColor = Color(0xFF000000);
+  static const darkInactiveColor = Color(0xFF8D8D8D);
+  static const darkFormColor = Color(0xFF6750A4);
+  static const darkFormErrorColor = Color(0xFFB3261E);
+  static const darkGoodColor = Color(0xFF8D8D8D);
+  static const darkMediumColor = Color(0xFFFFCA3A);
+  static const darkBadColor = Color(0xFFFF595E);
+
+  static const borderRadiusSmall = Radius.circular(4);
+  static const borderRadiusStandart = Radius.circular(8);
+  static const borderRadiusLarge = Radius.circular(16);
+
+  static const paddingBetweenSmall = 8.0;
+  static const paddingBetweenStandart = 16.0;
+  static const paddingBetweenLarge = 32.0;
+
+  static const fontSizeSecondary = 12.0;
+  static const fontSizeBody = 16.0;
+  static const fontSizeButton = 20.0;
+  static const fontSizeAppBarTitle = 24.0;
+  static const fontSizeBodyTitle = 32.0;
+
+  static const fontWeightLight = FontWeight.w200;
+  static const fontWeightStandart = FontWeight.normal;
+  static const fontWeightBold = FontWeight.bold;
+}
